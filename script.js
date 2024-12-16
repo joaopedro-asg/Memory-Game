d03c39cbf1acf996de30368a9594976a657e3948
kleber-luc <kleberlucasalvessilva@gmail.com>
2024-12-16 19:50:24 -0300

    script.js

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
-// Seleciona todas as cartas
-const cards = document.querySelectorAll('.memory-card');
+function disableCards() {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
 
-// Função para embaralhar as cartas
-function shuffleCards() {
-    const memoryGame = document.querySelector('.memory-game');
-    const shuffledCards = Array.from(cards);
- // Embaralhamento usando Fisher-Yates
-    for (let i = shuffledCards.length - 1; i > 0; i--) {
-        const randomIndex = Math.floor(Math.random() * (i + 1));
-        [shuffledCards[i], shuffledCards[randomIndex]] = [shuffledCards[randomIndex], shuffledCards[i]];
-    }
+            resetBoard();
+        }
 
-    // Remove todas as cartas do DOM //
-    shuffledCards.forEach(card => memoryGame.removecads(card));
+        function unflipCards() {
+            lockBoard = true;
 
-    // Adiciona as cartas embaralhadas de volta ao DOM //
-    shuffledCards.forEach(card => memoryGame.appendcards(card));
-}
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+                resetBoard();
+            }, 1000);
+        }
 
-// Função para organizar as cartas na ordem original //
-function resetCards() {
-    const memoryGame = document.querySelector('.memory-game');
-    const sortedCards = Array.from(cards).sort((a, b) => {
-        const charA = a.getAttribute('data-char');
-        const charB = b.getAttribute('data-char');
-        return charA.localeCompare(charB);
-    });
-
-// Remove todas as cartas do DOM
-    sortedCards.forEach(card => memoryGame.removecards(card));
-
-    // Adiciona as cartas organizadas de volta ao DOM
-    sortedCards.forEach(card => memoryGame.appendcards(card));
-}
-
-// Executa o embaralhamento ao carregar a página
-shuffleCards();
+        function resetBoard() {
+            [firstCard, secondCard, lockBoard] = [null, null, false];
+        }
