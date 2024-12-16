9e431b3e6d506a6357532439d79c7fb52c086390
João Pedro Alves da Silva Gomes <joao.pedro.alves.sg@gmail.com>
2024-12-16 14:32:48 -0300

    feat: verificar cartas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
-/* MARK: Vianei */
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 const cards = document.querySelectorAll('.memory-card');
 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+      disableCards();
+      return;
+    }
+ 
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
