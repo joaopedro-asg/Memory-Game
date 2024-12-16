881f2ffe2390d594422e1649798c1e6563b80567
João Pedro Alves da Silva Gomes <joao.pedro.alves.sg@gmail.com>
2024-12-16 11:40:13 -0300

    feat: interação com a carta

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* MARK: Vianei */
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
