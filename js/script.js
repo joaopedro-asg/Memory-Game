b1079444b44da6ea16c2aa3a9e62697717d62e48
Ingryd <ingrydcduarte@gmail.com>
2024-12-19 18:47:45 -0300

    fix: reset game and board

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ let firstCard, secondCard;
 
 const cards = document.querySelectorAll('.memory-card');
 
+//MARK: - Verifica se a carta já foi virada e se não foi, é feito.
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
 
     this.classList.add('flip');
-    
+
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
@@ -17,19 +18,21 @@ function flipCard() {
     }
 
     secondCard = this;
-
     checkForMatch();
 }
 
+//MARK: Verifica se as cartas são iguais, se não, desvira
 function checkForMatch() {
     if (firstCard.dataset.char === secondCard.dataset.char) {
-      disableCards();
-      return;
+        disableCards(); 
+        checkAllCardsFlipped();
+        return;
     }
- 
+
     unflipCards();
 }
 
+//MARK: Após a carta estar virada, desabilita para virar para baixo
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
@@ -37,27 +40,54 @@ function disableCards() {
     resetBoard();
 }
 
+//MARK: Desvira as cartas após um tempo
 function unflipCards() {
     lockBoard = true;
-    
-    setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
 
-      resetBoard();
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
 }
 
+//MARK: Reseta as cartas
 function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle() {
-    cards.forEach(card => {
-      let ramdomPos = Math.floor(Math.random() * 10);
-      card.style.order = ramdomPos;
-    });
-})();
+// MARK: - Função que verifica se todas as cartas deram match
+function checkAllCardsFlipped() {
+    const allFlipped = Array.from(cards).every(card => card.classList.contains('flip')); //aqui ele verifica se todas as cartas estão viradas, caso for verdadeira
+    
+    if (allFlipped) {
+        setTimeout(resetGame, 2000); //se for verdadeiro, reinicia tudo depois de um delay de 2s (ms)
+    }
+}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+//MARK: Reseta o jogo
+function resetGame() {
+    cards.forEach(card => {
+        card.classList.remove('flip');
+        card.addEventListener('click', flipCard);
+    });
+
+    resetBoard();
+    shuffle();
+}
+
+// MARK: - Embaralha as cartas
+function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * cards.length);
+        card.style.order = randomPos;
+    });
+}
+
+// MARK: - Inicio
+(function initializeGame() {
+    cards.forEach(card => card.addEventListener('click', flipCard));
+    shuffle();
+})();
